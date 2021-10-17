8ff6338fb13005588dc65ee62e790f9fcb24f288
saache99 <91719995+saache99@users.noreply.github.com>
2021-10-17 17:57:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload()
+{
+    clound = loadImage('https://i.postimg.cc/Gh1LnNmS/l1.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelReady);
+    poseNet.on('pose',getPoses);
+}
+function draw()
+{
+ image(video,0,0,300,300);
+ image(clound,nose_x+40,nose_y,30,30);
+}
+function save_it()
+{
+    save('myFilterImage.png');
+}
+function modelReady()
+{
+    console.log("Ready");
+}
+function getPoses(results)
+{
+    if(results.length > 0)
+    {
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+
+        console.log("x" + nose_x);
+        console.log("y" + nose_y);
+    }
+}
